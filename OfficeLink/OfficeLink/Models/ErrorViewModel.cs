e0b7bf8fb437910d77e11927e3bb603d7a66cad7
Quang <quangnguyenvietminh@gmail.com>
2019-05-21 15:37:39 +0700

    Init Project Commit

---
+++ OfficeLink/OfficeLink/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OfficeLink.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
